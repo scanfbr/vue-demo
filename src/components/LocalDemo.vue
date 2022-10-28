66524a14cc8530e169abb806fb36436b99898a28
fzpc <971324219@qq.com>
2022-10-28 10:19:44 +0800

    1

---
+++ src/components/LocalDemo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="box">
+        <div class="main">
+            <button class="get_btn" @click="getList">你好</button>
+        </div>
+    </div>
+
+</template>
+<script>
+
+
+export default{
+    name:'LocalDemo',
+    data() {
+        return {
+            title:'这里是TITLE',
+            count: 1
+        }
+    },
+    methods:{
+        getList(){
+            this.$Axios({
+                method: 'get',
+                url:'',
+                params:{
+                    limit: 1
+                }
+            }).then(res =>{
+                console.log(res)
+            })
+        }
+    }
+}
+</script>
